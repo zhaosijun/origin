fe3793c8996d1905a685247eeb6de6981765cf12
zhaosijun <zhao.sijun@zte.com.cn>
2016-10-09 16:34:31 +0800

    Update volume_metric_of_world_provider.go

--- vendor/k8s.io/kubernetes/pkg/kubelet/volumemanager/provider/volume_metric_of_world_provider.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/volumemanager/provider/volume_metric_of_world_provider.go
@@ -40,8 +40,12 @@ import (
 	"k8s.io/kubernetes/pkg/volume/util/volumehelper"
 )
 
-// The maximum number of routine running `du` tasks at once.
-const maxConsecutiveRoutinesRunningDu = 20
+const (
+	longDu                          = 1 * time.Second
+	maxDuBackoffFactor              = 20
+	// The maximum number of routine that is running `du` tasks at once.
+	maxConsecutiveRoutinesRunningDu = 20
+)
 
 // A pool for restricting the number of consecutive routines running `du` tasks.
 var routinePoolForRunningDu = make(chan struct{}, maxConsecutiveRoutinesRunningDu)
@@ -61,26 +65,28 @@ func releaseRoutineTokenForRunningDu() {
 }
 
 
-// DesiredStateOfWorldPopulator periodically loops through the list of active
-// pods and ensures that each one exists in the desired state of the world cache
-// if it has volumes. It also verifies that the pods in the desired state of the
-// world cache still exist, if not, it removes them.
+// VolumesMetricsOfWorldProvider periodically loops through the map containing  
+// metrics providers which generated from actual world cache and update each 
+// volume by excuting 'du' on the path to which the volume should be mounted for
+// a pod.
 type VolumesMetricsOfWorldProvider interface {
+	// Starts running the provider loop which executes periodically, checks
+	// if volume metrics that should be updated will be measured by excuting 'du'.
 	Run(stopCh <-chan struct{})
 }
 
-// NewDesiredStateOfWorldPopulator returns a new instance of
-// DesiredStateOfWorldPopulator.
+// NewVolumesMetricsOfWorldProvider returns a new instance of
+// NewVolumesMetricsOfWorldProvider.
 //
-// kubeClient - used to fetch PV and PVC objects from the API server
-// loopSleepDuration - the amount of time the populator loop sleeps between
+// metricsCacheDuration - the amount of time the metrics data is effective
+// 	in the cache
+// loopSleepDuration - the amount of time the provider loop sleeps between
 //     successive executions
-// podManager - the kubelet podManager that is the source of truth for the pods
-//     that exist on this host
-// desiredStateOfWorld - the cache to populate
+// actualStateOfWorld - the cache of actual world
+// volumesMetricsOfWorld - the cache to keep volume metrics data
 func NewVolumesMetricsOfWorldProvider(
+	metricsCacheDuration time.Duration,
 	loopSleepDuration time.Duration,
-	getPodStatusRetryDuration time.Duration,
 	actualStateOfWorld cache.ActualStateOfWorld,
 	volumesMetricsOfWorld cache.VolumesMetricsOfWorld) VolumesMetricsOfWorldProvider {
 	return &volumesMetricsOfWorldProvider{
@@ -98,23 +104,40 @@ type volumesMetricsOfWorldProvider struct {
 	volumesMetricsOfWorld     cache.VolumesMetricsOfWorld
 }
 
+// startMeasureVolume start VolumeMetricsProvider to update volume metrics.
+// cacheDuration is from the volume metrics data in cache. 
 func (vmwp *volumesMetricsOfWorldProvider) startMeasureVolume(metricsProvider *VolumeMetricsProvider, cacheDuration time.Duration) {
+	volumeName = metricsProvider.GetVolumeName()
+	// It should mark the volume is being measured before take a Token.
+	// Otherwise paths to which the same volume is mounted may take token.
 	vmwp.volumesMetricsOfWorld.MarkVolumeMeasuringStatus(volumeName, true)
+	// startMeasureVolume Call will block here until the number of 'du' task 
+	// running at once is less than maxConsecutiveRoutinesRunningDu. 
 	claimRoutineTokenForRunningDu()
 	defer releaseRoutineTokenForRunningDu()
 	
-	volumeName = metricsProvider.GetVolumeName()
 	start := time.Now()
-	//Call GetMetrics will block
-	metrics := metricsProvider.GetMetrics()
-	duration := time.Since(start)
-	if duration > cacheDuration {
-		glog.V(2).Infof("`du` on volume with name %s took %v", volume, duration)
-		vmwp.volumesMetricsOfWorld.SetVolumeMetricsData(volumeName, metrics, false, duration*2, time.Now())
-	} else {
-		vmwp.volumesMetricsOfWorld.SetVolumeMetricsData(volumeName, metrics, false, cacheDuration , time.Now())
+	// GetMetrics Call maybe block here for some time.
+	metrics, err := metricsProvider.GetMetrics()
+	// If fail to get the volume metrics, double the cache time.
+	// The cache time is not greater than maxDuBackoffFactor*vmwp.metricsCacheDuration.
+	// Just as cadvisor does.
+	if err != nil {
+		glog.Errorf("failed to excute 'du' on volume with name %s - %v", volumeName, err)
+		cacheDuration = cacheDuration*2
+		if cacheDuration > maxDuBackoffFactor*vmwp.metricsCacheDuration {
+			cacheDuration = maxDuBackoffFactor*vmwp.metricsCacheDuration
+		}
+		vmwp.volumesMetricsOfWorld.SetVolumeMetricsData(volumeName, metrics, false, cacheDuration, time.Now())
+		return
 	}
 	
+	duration := time.Since(start)
+	if duration > longDu {
+		glog.V(2).Infof("`du` on volume with name %s took %v", volumeName, duration)
+	}
+	
+	vmwp.volumesMetricsOfWorld.SetVolumeMetricsData(volumeName, metrics, false, vmwp.metricsCacheDuration, time.Now())
 }
 
 func (vmwp *volumesMetricsOfWorldProvider) Run(stopCh <-chan struct{}) {
@@ -127,9 +150,14 @@ func (vmwp *volumesMetricsOfWorldProvider) Run(stopCh <-chan struct{}) {
 			volumeMetricsProviders := vmwp.actualStateOfWorld.GetMountedVolumeMetricsProviders()
 			for path, _ := range volumeMetricsProviders {
 				metricsProvider = volumeMetricsProviders[path]
-				volumeMetrics, measureRequired, exist := vmwp.volumesMetricsOfWorld.GetVolumeMetricsData(metricsProvider.GetVolumeName())
-				if !exist || measureRequired {
-					go vmwp.startMeasureVolume(metricsProvider, metricsCacheDuration)
+				//A volume mabye mounted to multiple paths. Excute 'du' on one.    
+				volumeMetrics, measureRequired, cacheDuration := vmwp.volumesMetricsOfWorld.GetVolumeMetricsData(metricsProvider.GetVolumeName())
+				//Set cache duration of new volume metrics as value 'vmwp.metricsCacheDuration'.
+				if volumeMetrics == nil {
+					cacheDuration = vmwp.metricsCacheDuration
+				}
+				if measureRequired {
+					go vmwp.startMeasureVolume(metricsProvider, cacheDuration)
 				}
 			}
 		}
