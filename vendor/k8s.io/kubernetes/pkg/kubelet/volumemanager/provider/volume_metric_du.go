3cdcdc1895776703fee22bd040418a90974bdebf
zhaosijun <zhao.sijun@zte.com.cn>
2016-10-09 16:43:17 +0800

    Update volume_metric_du.go

--- vendor/k8s.io/kubernetes/pkg/kubelet/volumemanager/provider/volume_metric_du.go
+++ vendor/k8s.io/kubernetes/pkg/kubelet/volumemanager/provider/volume_metric_du.go
@@ -15,7 +15,7 @@ limitations under the License.
 Package cache implements data structures used by the kubelet volume manager to
 keep track of attached volumes and the pods that mounted them.
 */
-package cache
+package provider
 
 import (
 	"errors"
@@ -25,14 +25,24 @@ import (
 	"k8s.io/kubernetes/pkg/volume/util"
 )
 
+const duTimeout = 1 * time.Minute
+
+// VolumeMetricsProvider exposes volume metrics and volume name
+// (e.g. used,available space) related to a volume.
 type VolumeMetricsProvider interface {
+	// GetMetrics calculates the volume usage and available space by executing "du".
 	GetMetrics() (*Metrics, error)
+	
 	GetVolumeName() api.UniqueVolumeName
 }
 
+// volumeMetricsDu represents a VolumeMetricsProvider that calculates 
+// the used and available Volume space by executing the "du" command.
 type volumeMetricsDu struct {
+	// the directory path the volume is mounted to.
 	path       string
 	volumeName api.UniqueVolumeName
+	// capacity is from volume spec.
 	capacity   *Quantity
 }
 
@@ -43,23 +53,20 @@ func NewVolumeMetricsDu(path string, volumeName api.UniqueVolumeName, podName ty
 				capacity:   capacity}
 }
 
-// GetMetrics calculates the volume usage and device free space by executing "du"
-// and gathering filesystem info for the Volume path.
-// See MetricsProvider.GetMetrics
 func (vmd *volumeMetricsDu) GetMetrics() (*Metrics, error) {
 	metrics := &Metrics{}
 	if vmd.path == "" || vmd.capacity == nil {
-		return metrics, errors.New("no path defined for volume with name %s.", vmd.volumeName)
+		return nil, fmt.Errorf("no path defined for volume with name %s.", vmd.volumeName)
 	}
 
 	err := vmd.runDu(metrics)
 	if err != nil {
-		return metrics, err
+		return nil, err
 	}
 
 	err := vmd.caculateVolumeMetrics(metrics)
 	if err != nil {
-		return metrics, err
+		return nil, err
 	}
 
 	return metrics, nil
@@ -71,7 +78,7 @@ func (vmd *volumeMetricsDu) GetVolumeName() api.UniqueVolumeName {
 
 // runDu executes the "du" command and writes the results to metrics.Used
 func (vmd *volumeMetricsDu) runDu(metrics *Metrics) error {
-	used, err := util.Du(vmd.path)
+	used, err := util.DuWithCheckingTimeout(vmd.path, duTimeout)
 	if err != nil {
 		return err
 	}
@@ -79,6 +86,7 @@ func (vmd *volumeMetricsDu) runDu(metrics *Metrics) error {
 	return nil
 }
 
+// caculateVolumeMetrics caculate volume availabel space.
 func (vmd *volumeMetricsDu) caculateVolumeMetrics(metrics *Metrics) error {
 	if metrics == nil || metrics.Used == nil {
 		return fmt.Errorf("Fail to caculete metrics of volume with name %s due to unknow used of metrics.", vmd.volumeName)
